df4621074c62974bcb89e23b8b51abababf4452a
Kozlov Nikita <52751608+Lranger321@users.noreply.github.com>
2025-04-13 22:10:27 +0600

    Добавил свагер
    * Добавил свагер
    
    * Добавил свагер
    
    ---------
    
    Co-authored-by: Lranger <k.nikitaigorevich@gmail.com>

--- src/main/java/ucc/controller/ClientController.java
+++ src/main/java/ucc/controller/ClientController.java
@@ -1,5 +1,8 @@
 package ucc.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +13,26 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(
+        name = "UCC Statistics API",
+        description = "API для получения статистики игроков UCC"
+)
 public class ClientController {
 
     private final MatchService matchService;
 
+    @Operation(
+            summary = "Получить статистику всех игроков",
+            description = "Возвращает список статистик по всем участникам UCC"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Успешный запрос"
+    )
+    @ApiResponse(
+            responseCode = "500",
+            description = "Внутренняя ошибка сервера"
+    )
     @GetMapping("/api/ucc/getStat")
     public List<PlayerStat> getPlayerStat() {
         return matchService.getPlayersStat();
--- src/main/java/ucc/dto/PlayerStat.java
+++ src/main/java/ucc/dto/PlayerStat.java
@@ -1,6 +1,7 @@
 package ucc.dto;
 
 import com.riot.api.model.GameMode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,10 @@ import java.util.Map;
 @Builder
 public class PlayerStat {
 
+    @Schema(description = "Имя игрока", example = "Lranger")
     private String playerName;
+    @Schema(description = "Титул игрока", example = "Feed дня")
     private String title;
+    @Schema(description = "Статистика матчей")
     private Map<GameMode, PlayerStatForGame> games;
 }
